81bd6e77a15d2145027abb83e4f45f4983c02eaf
ossobmx27-bit <osso.bmx27@gmail.com>
2026-07-30 17:57:38 +0900

    Create product detail page structure

---
+++ js/product.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("product-content").innerHTML = `
+
+        <h1>PRODUCT PAGE</h1>
+
+        <p>Loading...</p>
+
+    `;
+
+});
