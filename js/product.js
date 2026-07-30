ba62fb0818b33b627a068232597c5014d0aa0fc4
ossobmx27-bit <osso.bmx27@gmail.com>
2026-07-30 17:59:48 +0900

    Read product ID from URL

--- js/product.js
+++ js/product.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
 
+    // URLからidを取得
+    const params = new URLSearchParams(window.location.search);
+
+    const id = params.get("id");
+
     document.getElementById("product-content").innerHTML = `
 
         <h1>PRODUCT PAGE</h1>
 
-        <p>Loading...</p>
+        <p>ID : ${id}</p>
 
     `;
 
